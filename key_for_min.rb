719da3c2bce79ed785199f52237d21df14078e8e
Mark A Guza <guzamaguza@gmail.com>
2020-01-26 05:22:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,8 @@ def key_for_min_value(name_hash)
   if name_hash.length == 0
     return nil
   else
-    max_val = name_hash.value(name_hash.values.max)
-    #max_val = name_hash.max_by {|k,v| v}
+    #max_val = name_hash.value(name_hash.values.max)
+    max_val = name_hash.max_by {|k,v| v}
     name_hash.each do |symbol, value|
       if value < max_val
         min_value = value
